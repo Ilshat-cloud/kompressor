8c806976e814cd3281ca197f967dfbc3ac80454e
Ilshat-cloud <64308014+Ilshat-cloud@users.noreply.github.com>
2021-02-03 22:31:34 +0300

    Add files via upload

--- main.h
+++ main.h
@@ -50,6 +50,8 @@ extern "C" {
 
 /* USER CODE END EM */
 
+void HAL_TIM_MspPostInit(TIM_HandleTypeDef *htim);
+
 /* Exported functions prototypes ---------------------------------------------*/
 void Error_Handler(void);
 
@@ -58,40 +60,34 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define Voltage_Pin GPIO_PIN_0
-#define Voltage_GPIO_Port GPIOA
-#define T1_Pin GPIO_PIN_1
-#define T1_GPIO_Port GPIOA
-#define T2_Pin GPIO_PIN_2
-#define T2_GPIO_Port GPIOA
-#define Pressure_Pin GPIO_PIN_3
-#define Pressure_GPIO_Port GPIOA
-#define Current_A_Pin GPIO_PIN_4
-#define Current_A_GPIO_Port GPIOA
-#define Current_B_Pin GPIO_PIN_5
-#define Current_B_GPIO_Port GPIOA
-#define Current_C_Pin GPIO_PIN_6
-#define Current_C_GPIO_Port GPIOA
-#define Phase_A_Pin GPIO_PIN_0
-#define Phase_A_GPIO_Port GPIOB
-#define Phase_B_Pin GPIO_PIN_1
-#define Phase_B_GPIO_Port GPIOB
-#define Phase_C_Pin GPIO_PIN_2
-#define Phase_C_GPIO_Port GPIOB
-#define SPI2_CS_Pin GPIO_PIN_12
-#define SPI2_CS_GPIO_Port GPIOB
-#define Start_solenoid_Pin GPIO_PIN_3
-#define Start_solenoid_GPIO_Port GPIOB
-#define ENCODER_1_Pin GPIO_PIN_4
-#define ENCODER_1_GPIO_Port GPIOB
-#define ENCODER_2_Pin GPIO_PIN_5
-#define ENCODER_2_GPIO_Port GPIOB
-#define TIM4_Ch1_BTN_Pin GPIO_PIN_6
-#define TIM4_Ch1_BTN_GPIO_Port GPIOB
-#define START_Pin GPIO_PIN_7
-#define START_GPIO_Port GPIOB
+#define TEMPERATURE_1_Pin GPIO_PIN_0
+#define TEMPERATURE_1_GPIO_Port GPIOA
+#define PWM_LED_Pin GPIO_PIN_2
+#define PWM_LED_GPIO_Port GPIOA
+#define Btn_Pin GPIO_PIN_5
+#define Btn_GPIO_Port GPIOA
+#define ENCODER_1_Pin GPIO_PIN_6
+#define ENCODER_1_GPIO_Port GPIOA
+#define ENCODER_2_Pin GPIO_PIN_7
+#define ENCODER_2_GPIO_Port GPIOA
+#define SPI2_Pin GPIO_PIN_15
+#define SPI2_GPIO_Port GPIOB
+#define PID_PWM_OUT_Pin GPIO_PIN_15
+#define PID_PWM_OUT_GPIO_Port GPIOA
+#define BREIZ_IN_EXTI_Pin GPIO_PIN_3
+#define BREIZ_IN_EXTI_GPIO_Port GPIOB
+#define BREIZ_OUT_Pin GPIO_PIN_4
+#define BREIZ_OUT_GPIO_Port GPIOB
+#define RELAY_HIST_Pin GPIO_PIN_5
+#define RELAY_HIST_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
+//page 16- 8000-8800
 
+  static const int   User_Page_Adress[]={
+  0x0800FC00,0x0800FC04,0x0800FC08,0x0800FC0C,0x0800FC10,0x0800FC14,0x0800FC18,0x0800FC1C,
+  0x0800FC20,0x0800FC24,0x0800FC28,0x0800FC2C,0x0800FC30,0x0800FC34,0x0800FC38,0x0800FC3C,
+  0x0800FC40,0x0800FC44,0x0800FC48,0x0800FC4C,0x0800FC50,0x0800FC54,0x0800FC58,0x0800FC5C,
+  0x0800FC60,0x0800FC64};
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
