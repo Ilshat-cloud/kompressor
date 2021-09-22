c9bd2c9bcb6ebb05964af45b686abeca4b783cd3
Ilshat-cloud <64308014+Ilshat-cloud@users.noreply.github.com>
2021-09-22 22:04:46 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -25,6 +25,16 @@
 /* USER CODE BEGIN Includes */
 #include "sh1106.h"
 #include "task.h"
+//#include "socket.h"
+//#include "mqtt_interface.h"
+//#include "MQTTClient.h"
+
+//#include "socket.c"
+
+//#define TCP_SOCKET     0
+//#define PORT_TCPS       5000
+//#define DATA_BUF_SIZE   2048
+//uint8_t gDATABUF[DATA_BUF_SIZE];
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -127,6 +137,60 @@ RTC_DateTypeDef CurDate={0};
 uint32_t button=0,worktime,flash_ret;
 FLASH_EraseInitTypeDef Erase;
 //========================================//
+/*
+//-----------------------------------------------------------------------------------------temp!!!!!---------------------------------------------------------------//
+wiz_NetInfo gWIZNETINFO = { .mac = {0x00, 0x08, 0xdc, 0xab, 0xcd, 0xef},
+                            .ip = {192, 168, 0, 2},
+                            .sn = {255, 255, 255, 0},
+                            .gw = {192, 168, 0, 1},
+                            .dns = {0, 0, 0, 0},
+                            .dhcp = NETINFO_STATIC };
+
+void W5500_Select(void)
+{
+    HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, GPIO_PIN_RESET);
+}
+
+void W5500_Unselect(void) 
+{
+    HAL_GPIO_WritePin(SPI2_CS_GPIO_Port, SPI2_CS_Pin, GPIO_PIN_SET);
+}
+
+void W5500_ReadBuff(uint8_t* buff, uint16_t len) 
+{
+    HAL_SPI_Receive(&hspi2, buff, len, HAL_MAX_DELAY);
+}
+
+void W5500_WriteBuff(uint8_t* buff, uint16_t len) 
+{
+    HAL_SPI_Transmit(&hspi2, buff, len, HAL_MAX_DELAY);
+}
+
+uint8_t W5500_ReadByte(void) 
+{
+    uint8_t byte;
+    W5500_ReadBuff(&byte, sizeof(byte));
+    return byte;
+}
+
+void W5500_WriteByte(uint8_t byte) 
+{
+    W5500_WriteBuff(&byte, sizeof(byte));
+}
+
+void messageArrived(MessageData* md)
+{
+  MQTTMessage* message = md->message;
+  for (uint8_t i = 0; i < md->topicName->lenstring.len; i++)
+  putchar(*(md->topicName->lenstring.data + i));
+
+}
+//-----------------------------------------------------------------------------------------!---------------------------------------------------------------//
+
+*/
+
+
+
 /* USER CODE END 0 */
 
 /**
@@ -2071,10 +2135,67 @@ void Screen(void *argument)
 void MQTT(void *argument)
 {
   /* USER CODE BEGIN MQTT */
+  /*
+  HAL_GPIO_WritePin(W5500_RST_GPIO_Port, W5500_RST_Pin, GPIO_PIN_RESET);
+  osDelay(1);
+  HAL_GPIO_WritePin(W5500_RST_GPIO_Port, W5500_RST_Pin, GPIO_PIN_SET);
+  osDelay(1000);
+  uint8_t buf[100];
+  uint32_t rc,mes_id;
+  
+  reg_wizchip_cs_cbfunc(W5500_Select, W5500_Unselect);
+  reg_wizchip_spi_cbfunc(W5500_ReadByte, W5500_WriteByte);
+  reg_wizchip_spiburst_cbfunc(W5500_ReadBuff, W5500_WriteBuff);
+	
+  uint8_t rx_tx_buff_sizes[] = {2, 2, 2, 2, 2, 2, 2, 2};
+  wizchip_init(rx_tx_buff_sizes, rx_tx_buff_sizes);
+  wizchip_setnetinfo(&gWIZNETINFO);
+  ctlnetwork(CN_SET_NETINFO, (void*) &gWIZNETINFO);
+  osDelay(1000);
+   
+  Network n;
+  MQTTClient c;
+  n.my_socket = 0;
+  uint8_t targetIP[4] = {192, 168, 0, 1};   //brocker IP adress
+  
+  NewNetwork(&n, TCP_SOCKET);
+  ConnectNetwork(&n, targetIP, 1883);
+  MQTTClientInit(&c, &n, 1000, buf, 100, gDATABUF, DATA_BUF_SIZE);
+
+//connecting 
+  MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+  data.willFlag = 0;
+  data.MQTTVersion = 3;//4;
+  data.clientID.cstring = (char*)"w5500-client";
+  data.username.cstring = "username";
+  data.password.cstring = "";
+  
+  data.keepAliveInterval = 60;
+  data.cleansession = 1;
+ 
+  rc = MQTTConnect(&c, &data);
+  //subscribing
+  rc = MQTTSubscribe(&c, "ss/#", QOS0, messageArrived);
+  
+  
+  */
+  
+  
+
   /* Infinite loop */
   for(;;)
   {
-    osDelay(1);
+    
+      //  MQTTMessage pubMessage;
+      //  pubMessage.qos = QOS0;
+     //   pubMessage.id = mes_id++;
+     //   pubMessage.payloadlen = 12;
+     //   pubMessage.payload = "sasai_lalka";
+     //   MQTTPublish(&c, "/w5500_stm32_client", &pubMessage);
+
+	osDelay(1000);
+	//MQTTYield(&c, 1000);
+    
   }
   /* USER CODE END MQTT */
 }
